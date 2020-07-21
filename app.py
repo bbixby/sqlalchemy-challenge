32e5876886fe6cefa45eab22fb33ff431197c1cf
Matt Debnar <matthew.debnar@gmail.com>
2020-07-21 18:56:23 -0400

    fixed histogram and app.py database

--- app.py
+++ app.py
@@ -1,14 +1,29 @@
 #################################################
 # Import Flask
 #################################################
+import numpy as np
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
 from flask import Flask, jsonify
-import json
+
 #################################################
-# Import Data
+# Database Setup
 #################################################
-with open('precipitationDF.json') as json_file:
-    precipitationdata = json.load(json_file)
-#precipitationDF = [year_precipitation]
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+# reflect an existing database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Measurement = Base.classes.measurement
+Station = Base.classes.station
+
 
 
 #################################################
